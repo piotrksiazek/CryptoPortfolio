47969349f3c5dc1c206b00e90b576cb7e2581ced
Piotr <piotr.ksiazek.mw@gmail.com>
2022-02-14 16:11:41 +0100

    add repository base, interface

---
+++ Infrastructure/Data/Repositories/CryptocurrencyRepository.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class CryptocurrencyRepository : RepositoryBase<Cryptocurrency>, ICryptocurrencyRepository
+    {
+        public CryptocurrencyRepository(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ Infrastructure/Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,56 @@
+﻿using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Repositories
+{
+    public class RepositoryBase<T> : IRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+
+        public RepositoryBase(DbContext context)
+        {
+            Context = context;
+        }
+
+        public T Get(int id)
+        {
+            return Context.Set<T>().Find(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate);
+        }
+
+        public void Add(T entity)
+        {
+            Context.Set<T>().Add(entity);
+        }
+
+        public void AddRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().AddRange(entities);
+        }
+
+        public void Remove(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            Context.Set<T>().RemoveRange(entities);
+        }
+    }
+}
