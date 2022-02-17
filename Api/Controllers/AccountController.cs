d6a341c0751f404f93fedea34470f8c287e86e97
PiotrKsiazek <piotr.ksiazek.mw@gmail.com>
2022-02-17 19:51:54 +0100

    Add account controller with login and register

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using Api.Dtos;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>>Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized();
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized();
+
+            return new UserDto
+            {
+                Username = loginDto.Email,
+                Email = loginDto.Email,
+                Token = "Token"
+            };
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if(!result.Succeeded) return Unauthorized();
+
+            return new UserDto
+            {
+                Username = registerDto.Email,
+                Email  =registerDto.Email,
+                Token = "Token"
+            };
+        }
+
+
+
+    }
+}
