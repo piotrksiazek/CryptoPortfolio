2f3efc48db574aaa5e27571d93a76b36fb782ce2
PiotrKsiazek <piotr.ksiazek.mw@gmail.com>
2022-04-07 20:55:14 +0200

    Delete unused files and commented chunks

--- Tests/MailingServiceTests.cs
+++ Tests/MailingServiceTests.cs
@@ -19,12 +19,6 @@ namespace Tests
 {
     public class MailingServiceTests
     {
-        //private readonly IUnitOfWork _uow;
-
-        //public MailingServiceTests(IUnitOfWork uow)
-        //{
-        //    _uow = uow;
-        //}
         [Theory]
         [InlineData("CryptoPortfolioEmailPassword")]
         [InlineData("CryptoPortfolioEmailUsername")]
@@ -33,33 +27,6 @@ namespace Tests
             var actual = Environment.GetEnvironmentVariable(enviromentVariableName);
             Assert.NotNull(actual);
         }
-
-        //[Fact]
-        //public void SendEmailAsync_ShouldSucceed()
-        //{
-        //    //arrange
-        //    var mailingService = new MailingService();
-        //    bool didSend = false;
-        //    mailingService.HandleSuccessfulEmailDelegate = () => SuccesfulEmailReplacment(ref didSend);
-        //    mailingService.HandleFailedEmailDelegate = () => FailedEmailReplacment(ref didSend);
-
-        //    var uowMock = new Mock<IUnitOfWork>();
-
-        //    var email = new NotificationEmail();
-        //    email.Title = "title";
-        //    email.Body = "body";
-        //    email.Sender = Environment.GetEnvironmentVariable("CryptoPortfolioEmailUsername");
-        //    email.Reciever = Environment.GetEnvironmentVariable("CryptoPortfolioEmailUsername");
-
-        //    var notification = new Notification();
-
-        //    //act
-        //    mailingService.SendEmailAsync(email, notification, _uow);
-        //    Thread.Sleep(3000); //should be enough for email to be sent.
-
-        //    Assert.True(didSend);
-
-        //}
 
         private void SuccesfulEmailReplacment(ref bool didSend)
         {
